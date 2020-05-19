20986c6e3200de5b96a113d0235f314fba57ed82
r.beaumont <r.beaumont@criteo.com>
2020-05-19 18:45:07 +0200

    Release 1.0.0

--- check_version.py
+++ check_version.py
@@ -3,7 +3,7 @@ import subprocess
 _METADATA = dict(re.findall(r'__([a-z]+)__\s*=\s*"([^"]+)"', open("deepr/version.py").read()))
 version = _METADATA['version']
 process = subprocess.Popen(["git", "describe", "--tags"], stdout=subprocess.PIPE)
-tagged_version = process.communicate()[0].strip()
+tagged_version = process.communicate()[0].strip().decode(encoding="utf-8")
 if version == tagged_version:
     print(f"Tag and version are the same ({version}) !")
     exit(0)
