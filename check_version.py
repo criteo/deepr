50db45059f5e4a29b038aa21573f7144957eab33
r.beaumont <r.beaumont@criteo.com>
2020-05-19 16:09:30 +0200

    add github actions

---
+++ check_version.py
@@ -0,0 +1,12 @@
+import re
+import subprocess
+_METADATA = dict(re.findall(r'__([a-z]+)__\s*=\s*"([^"]+)"', open("deepr/version.py").read()))
+version = _METADATA['version']
+process = subprocess.Popen(["git", "describe", "--tags"], stdout=subprocess.PIPE)
+tagged_version = process.communicate()[0].strip()
+if version == tagged_version:
+    print(f"Tag and version are the same ({version}) !")
+    exit(0)
+else:
+    print(f"Tag {tagged_version} and version {version} are not the same !")
+    exit(1)
