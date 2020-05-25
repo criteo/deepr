9bf7b80c33a564904e4dd6d0ce512753bf0176a8
Guillaume Genthial <g.genthial@criteo.com>
2020-05-25 12:45:50 +0200

    Make example more complex, add advanced notebook

--- deepr/example/layers/model.py
+++ deepr/example/layers/model.py
@@ -14,4 +14,4 @@ LOGGER = logging.getLogger(__name__)
 @dprl.layer(inputs="x", outputs="y_pred")
 def Multiply(tensors):
     alpha = tf.get_variable(name="alpha", shape=(), dtype=tf.float32)
-    return alpha * tensors
+    return tf.multiply(alpha, tensors, name="y_pred")
