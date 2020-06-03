5c8758ac93da900dd3f9c921778174ac214d9476
Guillaume Genthial <guillaumegenthial@users.noreply.github.com>
2020-06-03 18:27:03 +0200

    Release 2.0.0 (#25)

--- deepr/version.py
+++ deepr/version.py
@@ -1,4 +1,4 @@
 # pylint: disable=all
 
-__version__ = "1.2.1"
+__version__ = "2.0.0"
 __author__ = "Criteo"
--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ copyright = "2020, Criteo"
 author = "Criteo"
 
 # The full version, including alpha/beta/rc tags
-release = "1.0.0"
+release = "2.0.0"
 
 
 # -- General configuration ---------------------------------------------------
