7bc599f2e205c8a766567347adf58db56beab9e4
Martin Bompaire <m.bompaire@criteo.com>
2020-07-02 18:33:31 +0200

    Release 2.6.0

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ copyright = "2020, Criteo"
 author = "Criteo"
 
 # The full version, including alpha/beta/rc tags
-release = "2.5.1"
+release = "2.6.0"
 
 
 # -- General configuration ---------------------------------------------------
