efa1a9075d3b3b43a9661edf27190199abb9c470
Guillaume Genthial <g.genthial@criteo.com>
2020-08-19 12:23:44 +0200

    Make arrays_to_example function importable

--- deepr/prepros/__init__.py
+++ deepr/prepros/__init__.py
@@ -4,4 +4,4 @@ from deepr.prepros.base import Prepro, PreproFn, prepro
 from deepr.prepros.combinators import Serial
 from deepr.prepros.core import Map, Filter, PaddedBatch, Shuffle, Repeat, Prefetch, Take, Batch, Cache
 from deepr.prepros.lookup import TableInitializer
-from deepr.prepros.record import TFRecordSequenceExample, FromExample, ToExample
+from deepr.prepros.record import TFRecordSequenceExample, FromExample, ToExample, arrays_to_example
