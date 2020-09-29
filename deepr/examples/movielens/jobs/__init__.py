a10405b30da8ca97334863cc6c795247e10efcda
Guillaume Genthial <g.genthial@criteo.com>
2020-09-29 11:53:47 +0200

    Add InitCheckpoint job to MovieLens

--- deepr/examples/movielens/jobs/__init__.py
+++ deepr/examples/movielens/jobs/__init__.py
@@ -4,3 +4,4 @@ from deepr.examples.movielens.jobs.build_records import BuildRecords
 from deepr.examples.movielens.jobs.build_csv import BuildCSV
 from deepr.examples.movielens.jobs.predict import Predict
 from deepr.examples.movielens.jobs.evaluate import Evaluate
+from deepr.examples.movielens.jobs.init import InitCheckpoint
