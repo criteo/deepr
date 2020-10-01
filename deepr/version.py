866b115eb9cad77de22fd85c03d74e927ad19e66
Guillaume Genthial <g.genthial@criteo.com>
2020-10-01 10:58:49 +0200

    Release 2.9.0

--- deepr/version.py
+++ deepr/version.py
@@ -1,4 +1,4 @@
 # pylint: disable=all
 
-__version__ = "2.8.2"
+__version__ = "2.9.0"
 __author__ = "Criteo"
--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ copyright = "2020, Criteo"
 author = "Criteo"
 
 # The full version, including alpha/beta/rc tags
-release = "2.8.2"
+release = "2.9.0"
 
 
 # -- General configuration ---------------------------------------------------
