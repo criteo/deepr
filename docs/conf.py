84de1e8beac7aa8bebfea371f29a229cb4aee002
Martin Bompaire <m.bompaire@criteo.com>
2020-10-28 18:22:02 +0100

    Bump version to 2.10.0

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ copyright = "2020, Criteo"
 author = "Criteo"
 
 # The full version, including alpha/beta/rc tags
-release = "2.9.1"
+release = "2.10.0"
 
 
 # -- General configuration ---------------------------------------------------
