6b9aee467bcfbdbb4bbe4ddbcdbcc866fcf76f7e
Guillaume Genthial <g.genthial@criteo.com>
2020-12-11 15:53:08 +0100

    Release 2.11.0

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ copyright = "2020, Criteo"
 author = "Criteo"
 
 # The full version, including alpha/beta/rc tags
-release = "2.10.0"
+release = "2.11.0"
 
 
 # -- General configuration ---------------------------------------------------
